f94f3ad2545f2dac772b7975bccfded6322a9787
Erbis Vladislav <cool.ru1960@gmail.com>
2025-05-06 00:31:25 +0300

    task 4

--- car.js
+++ car.js
@@ -5,3 +5,8 @@ let carOwner = "Elon Musk";
 alert(carName);
 alert(maxCarSpeed);
 alert(carOwner);
+
+maxCarSpeed = 350;
+console.log('У машины изменилась скорость', maxCarSpeed);
+carOwner = "John Doe";
+console.log('У машины изменился владелец', carOwner);
